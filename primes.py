10f898014969024c812b65fac248968cc1df08c3
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-13 15:26:20 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -4,20 +4,24 @@
 def primes(number_of_primes):
     list = []
     newlist = []
-    while 1:
-        for i in range(2, 100):
-            for j in range(2, i):
-                if j == i:
-                    pass
+    try:
+        while 1:
+            for i in range(2, 100):
+                for j in range(2, i):
+                    if j == i:
+                        pass
+                    else:
+                        if i%j == 0:
+                            list.append(i)
+                if i not in list:
+                    newlist.append(i)
+
+                if len(newlist) == int(number_of_primes):
+                    break
                 else:
-                    if i%j == 0:
-                        list.append(i)
-            if i not in list:
-                newlist.append(i)
-            
-            if len(newlist) == int(number_of_primes):
-                break
-            else:
-                continue
-        break
-    return newlist
+                    continue
+            break
+        return newlist
+    except ValueError:
+        print("Input a positive number of primes")
+        
