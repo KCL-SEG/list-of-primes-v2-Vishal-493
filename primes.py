b3758ac4861cf51c60ca17fdd22244deec5a854e
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-13 15:22:45 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    newlist = []
+    while 1:
+        for i in range(2, 100):
+            for j in range(2, i):
+                if j == i:
+                    pass
+                else:
+                    if i%j == 0:
+                        list.append(i)
+            if i not in list:
+                newlist.append(i)
+            
+            if len(newlist) == int(number_of_primes):
+                break
+            else:
+                continue
+        break
+    return newlist
